de24691d1e41edc622544a1353c7c877701b5d7f
Ichiro65437 <ichirore10@gmail.com>
2025-09-07 14:07:34 -0600

    se ajusto el script de carga

--- src/js/load.js
+++ src/js/load.js
@@ -1,10 +1,10 @@
 window.addEventListener('load',function(){
-    var nombre_label=document.getElementById('nombre_div');
+   /* var nombre_label=document.getElementById('nombre_div');
 
 
     nombre_label.addEventListener('click',function(){
         this.innerHTML='Ichiro65437';
         console.log('presionaste click');
     });
-    console.log('componentes cargados');
+    console.log('componentes cargados');*/
 });
